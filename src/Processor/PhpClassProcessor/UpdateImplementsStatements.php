ff85f2afc04b5ba0f48993b5ae2e7c65599077b7
Dmitry Kuznetsov <d@kuznetsov.io>
2019-04-13 20:08:03 +0300

    Добавлено расширение интерфейсов

---
+++ src/Processor/PhpClassProcessor/UpdateImplementsStatements.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types=1);
+
+namespace Octava\CodeGenerator\Processor\PhpClassProcessor;
+
+use PhpParser\Node\Stmt;
+use Psr\Log\LoggerInterface;
+use PhpParser\Parser;
+
+class UpdateImplementsStatements
+{
+    use GetClassTrait;
+
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+    /**
+     * @var Parser
+     */
+    protected $parser;
+
+    public function __construct(LoggerInterface $logger, Parser $parser)
+    {
+        $this->logger = $logger;
+        $this->parser = $parser;
+    }
+
+    /**
+     * @param Stmt[] $originStmts
+     * @param Stmt[] $templateStmts
+     * @return Stmt[]
+     */
+    public function __invoke(array $originStmts, array $templateStmts): array
+    {
+        $originClass = $this->getClassStatement($originStmts);
+        $templateClass = $this->getClassStatement($templateStmts);
+
+        if (!$originClass || !$templateClass) {
+            return $originStmts;
+        }
+
+        if (!$originClass->implements) {
+            $originClass->implements = $templateClass->implements;
+        } else {
+            /**
+             * @var $originInterface
+             */
+            foreach ($templateClass->implements as $templateInterface) {
+                $found = false;
+                foreach ($originClass->implements as $originInterface) {
+                    if ($templateInterface->toString() === $originInterface->toString()) {
+                        $found = true;
+                        break;
+                    }
+                }
+                if (!$found) {
+                    $originClass->implements[] = $templateInterface;
+                }
+            }
+        }
+
+        return $originStmts;
+    }
+}
---
+++ tests/Processor/PhpClassProcessor/UpdateImplementsStatementsTest.php
@@ -0,0 +1,169 @@
+<?php
+declare(strict_types=1);
+
+namespace Octava\Tests\CodeGenerator\Processor\PhpClassProcessor;
+
+use Octava\CodeGenerator\Processor\PhpClassProcessor\UpdateImplementsStatements;
+use PhpParser\Parser;
+use PhpParser\ParserFactory;
+use PhpParser\PrettyPrinter;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\NullLogger;
+
+class UpdateImplementsStatementsTest extends TestCase
+{
+    /**
+     * @var UpdateImplementsStatements
+     */
+    protected $processor;
+    /**
+     * @var Parser
+     */
+    protected $parser;
+    /**
+     * @var PrettyPrinter\Standard
+     */
+    protected $printer;
+
+    public function testUpdateImplementsStatementsWithoutTemplateImplements(): void
+    {
+        $originSource = <<<'PHP'
+<?php
+namespace App;
+
+class Classname implements OriginInterface
+{
+}
+PHP;
+
+        $templateSource = <<<'PHP'
+<?php
+namespace App;
+
+class Classname
+{
+}
+PHP;
+
+        $expectedSource = <<<'PHP'
+namespace App;
+
+class Classname implements OriginInterface
+{
+}
+PHP;
+
+        $actualSourceStmts = $this->processor->__invoke($this->parser->parse($originSource), $this->parser->parse($templateSource));
+        $actualSource = $this->printer->prettyPrint($actualSourceStmts);
+        $this->assertEquals($expectedSource, $actualSource);
+    }
+
+    public function testUpdateImplementsStatementsWithoutOriginImplements(): void
+    {
+        $originSource = <<<'PHP'
+<?php
+namespace App;
+
+class Classname
+{
+}
+PHP;
+
+        $templateSource = <<<'PHP'
+<?php
+namespace App;
+
+class Classname implements TemplateImplements
+{
+}
+PHP;
+
+        $expectedSource = <<<'PHP'
+namespace App;
+
+class Classname implements TemplateImplements
+{
+}
+PHP;
+
+        $actualSourceStmts = $this->processor->__invoke($this->parser->parse($originSource), $this->parser->parse($templateSource));
+        $actualSource = $this->printer->prettyPrint($actualSourceStmts);
+        $this->assertEquals($expectedSource, $actualSource);
+    }
+
+    public function testUpdateMultipleImplementsStatements(): void
+    {
+        $originSource = <<<'PHP'
+<?php
+namespace App;
+
+class Classname
+{
+}
+PHP;
+
+        $templateSource = <<<'PHP'
+<?php
+namespace App;
+
+class Classname implements TemplateImplements, OriginImplements
+{
+}
+PHP;
+
+        $expectedSource = <<<'PHP'
+namespace App;
+
+class Classname implements TemplateImplements, OriginImplements
+{
+}
+PHP;
+
+        $actualSourceStmts = $this->processor->__invoke($this->parser->parse($originSource), $this->parser->parse($templateSource));
+        $actualSource = $this->printer->prettyPrint($actualSourceStmts);
+        $this->assertEquals($expectedSource, $actualSource);
+    }
+
+
+    public function testUpdateImplementsStatements(): void
+    {
+        $originSource = <<<'PHP'
+<?php
+namespace App;
+
+class Classname implements OriginInterface, BaseInterface
+{
+}
+PHP;
+
+        $templateSource = <<<'PHP'
+<?php
+namespace App;
+
+class Classname implements TemplateInterface, BaseInterface, ExtraTemplateInterface
+{
+}
+PHP;
+
+        $expectedSource = <<<'PHP'
+namespace App;
+
+class Classname implements OriginInterface, BaseInterface, TemplateInterface, ExtraTemplateInterface
+{
+}
+PHP;
+
+        $actualSourceStmts = $this->processor->__invoke($this->parser->parse($originSource), $this->parser->parse($templateSource));
+        $actualSource = $this->printer->prettyPrint($actualSourceStmts);
+        $this->assertEquals($expectedSource, $actualSource);
+    }
+
+
+    protected function setUp(): void
+    {
+        $logger = new NullLogger();
+        $this->printer = new PrettyPrinter\Standard();
+        $this->parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
+        $this->processor = new UpdateImplementsStatements($logger, $this->parser);
+    }
+}
